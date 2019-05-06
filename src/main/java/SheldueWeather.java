82ed96324f3c000a12cff7cab5c26177d99b70f8
Dmitry Streltsov <streltcov.dm@gmail.com>
2019-05-06 16:40:04 +0300

    add some shit

--- src/main/java/SheldueWeather.java
+++ src/main/java/SheldueWeather.java
@@ -36,9 +36,10 @@ public class SheldueWeather {
 
     private Weather sendWeather() throws IOException{
         System.out.println("Send weather method");
-        Response response = service.getWeather(System.getenv("weather_token"),"Paris","7").execute();
+        Response response = service.getWeather().execute();
         System.out.println("Message: " + response.code() + " " + response.message());
         return (Weather) response.body();
+        //System.getenv("weather_token"),"Paris","7"
     }
 
     public Forecast getForecast(Weather weather){
--- src/main/java/api/ServiceWeather.java
+++ src/main/java/api/ServiceWeather.java
@@ -6,7 +6,8 @@ import retrofit2.http.Query;
 import weather.Weather;
 
 public interface ServiceWeather {
-    @GET("forecast.json")
-    Call<Weather> getWeather(@Query("key") String key, @Query("q") String q, @Query("dayssdf") String days);
+    @GET("forecast.json?key=7d675e39903f4485b9f110957190605&q=Moscow&days=7")
+    Call<Weather> getWeather();
 }
 //http://api.apixu.com/v1/forecast.json?key=7d675e39903f4485b9f110957190605&q=Moscow&days=7
+//@Query("key") String key, @Query("q") String q, @Query("dayssdf") String days
